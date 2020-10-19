7094b2c42c7a147da22f81361a87772dcb59d4f4
Catarina Fernandes <up201806610@fe.up.pt>
2020-10-19 17:05:14 +0100

    2. HTTP parameters

---
+++ TP3 - PHP/sum2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>LTW - sum2</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+        <h1> <?php echo $num1 = $_GET['num1']; ?> + 
+             <?php echo $num2 = $_GET['num2']; ?> =
+             <?php echo $num1 + $num2 ?>
+        </h1>
+        <a href="form2.html">New form</a>
+    </body>
+</html>
